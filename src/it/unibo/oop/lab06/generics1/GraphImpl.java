caae1ad0db76e297f44893b70d88c5c4b042ff66
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2020-11-07 16:43:40 +0100

    Complete the graph exercise

---
+++ src/it/unibo/oop/lab06/generics1/GraphImpl.java
@@ -0,0 +1,100 @@
+package it.unibo.oop.lab06.generics1;
+
+import java.util.*;
+
+
+public class GraphImpl<N> implements Graph<N> {
+	
+	private Map<N, Set<N>> graphMapper;
+
+	
+	public GraphImpl(Map<N, Set<N>> map) {
+		this.graphMapper = map;
+	}
+	
+	public GraphImpl() {
+		this(new HashMap<>());
+	}
+	
+	/**
+	 * Add a node in the graph
+	 * @param node to add in the graph
+	 */
+	public void addNode(N node) {
+		if(node != null && !this.graphMapper.containsKey(node)) {
+			this.graphMapper.put(node, new HashSet<>());
+		}
+	}
+
+	/**
+	 * Add an edge in the graph
+	 * @param source node in the graph
+	 * @param target node in the graph
+	 */
+	public void addEdge(N source, N target) {
+		if(source != null && target != null) {
+			this.addNode(source);
+			this.graphMapper.get(source).add(target);
+		}
+	}
+
+	/**
+	 * Return the node set of the graph
+	 * @return the node set of the graph
+	 */
+	public Set<N> nodeSet() {
+		return this.graphMapper.keySet();
+	}
+	
+	/**
+	 * Return all the linked nodes of a source
+	 * @return the set of nodes linked to the source
+	 */
+	public Set<N> linkedNodes(N node) {
+		return  this.graphMapper.getOrDefault(node, null);
+	}
+	
+	/**
+	 * Return the path between two nodes using the DFS Algorithm
+	 * @param source node in the graph
+	 * @param target node in the graph
+	 * @return the path from source to target
+	 */
+	public List<N> getPath(N source, N target) {
+
+		// Output list
+		List<N> outputList = new LinkedList<>();
+		
+		// DFS
+		findUtility(source, target, (LinkedList<N>) outputList, new HashMap<>());
+		
+		return outputList;
+	}
+
+	private void findUtility(N node, N target, LinkedList<N> list, Map<N, Boolean> exploredNodes) {
+		
+		exploredNodes.put(node, true);
+		
+		if(!list.contains(target)) {
+			list.add(node);
+		}
+		
+		if(node == target) {
+			return;
+		}
+		
+		Iterator<N> iterator = this.graphMapper.get(node).iterator();
+		while(iterator.hasNext()) {
+			N adjacentEdge = iterator.next();
+			if(!exploredNodes.getOrDefault(adjacentEdge,false)) {
+				findUtility(adjacentEdge, target, list, exploredNodes);
+			}
+		}
+		
+		if(!list.contains(target)) {
+			list.removeLast();
+		}
+	}
+	
+	
+}
